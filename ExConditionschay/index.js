65d4fa03efb5f6743873070b4f3e2162ccbc1065
Chaymae El Hayani <layani.chay@gmail.com>
2024-03-07 08:42:48 +0100

    Upload file

---
+++ ExConditionschay/index.js
@@ -0,0 +1,16 @@
+let a = 1;
+let b = 2;
+let c = 2;
+
+if(a === b && a === c){
+  console.log("The three variables are the same");
+}
+else if (a === b || a === c || b===c ){
+  console.log("Two of the variables are the same");
+}
+else{
+  console.log("all variables are different");
+}
+
+
+
